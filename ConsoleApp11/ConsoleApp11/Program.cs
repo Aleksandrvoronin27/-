ef116ff90cc73c48f81ef832166627829cbd0500
Aleksandrvoronin27 <146413617+Aleksandrvoronin27@users.noreply.github.com>
2024-01-22 16:20:01 +0500

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в калькулятор");
+            double a, b, c;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("выберите действие +,-,*,/");
+            c = Convert.ToChar(Console.ReadLine());
+            switch (c)
+            {
+                case '+': c = a+b; break;
+                case '-': c = a - b; break;
+                case '*': c = a * b; break;
+                case '/':
+                    
+                    if (b != 0) 
+                    {
+                        c = a / b;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, деление на ноль");
+                        return; 
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неверная операция");
+                    return;
+
+                
+            }
+            Console.WriteLine("ОТВЕТ="+c);
+            Console.ReadKey();
+
+        }
+    }
+}
